333e445af4dd2796ff1428928dea902f3a4ed025
teddyxiong53 <teddyxiong53@users.noreply.github.com>
2022-10-30 14:00:12 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="teddyxiong53"
+original_name="flask_demo"
+original_urlname="flask-demo"
+original_description="Awesome flask_demo created by teddyxiong53"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv flask_demo $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- Containerfile
+++ Containerfile
@@ -2,8 +2,8 @@ FROM python:3.7-alpine
 COPY . /app
 WORKDIR /app
 RUN pip install .
-RUN project_name create-db
-RUN project_name populate-db
-RUN project_name add-user -u admin -p admin
+RUN flask_demo create-db
+RUN flask_demo populate-db
+RUN flask_demo add-user -u admin -p admin
 EXPOSE 5000
-CMD ["project_name", "run"]
+CMD ["flask_demo", "run"]
